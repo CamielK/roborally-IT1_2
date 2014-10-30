edae3b91e1b3489fe5bf76591a0e974627c5bf49
2 <2>
2014-10-30 14:11:07 +0100

    hoofdmenu .cs en .axml aangemaakt. probleem: SetContentView herkent hoofdmenu.axml niet

--- hoofdmenu.cs
+++ hoofdmenu.cs
@@ -1,27 +1,30 @@
-﻿using System;
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
-using Android.OS;
 
 namespace roborally
 {
-	[Activity (Label = "Roborally", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity
+	[Activity (Label = "hoofdmenu")]			
+	public class hoofdmenu : Activity
 	{
-
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
 			// koppel activity aan layout bestand
-			SetContentView (Resource.Layout.Main);
+			SetContentView (Resource.Layout.hoofdmenu);
 
 			// button ophalen
-			Button button = FindViewById<Button> (Resource.Id.myButton);
+			Button button = FindViewById<Button> (Resource.Id.uitloggen);
 
 			// button click gaat naar hoofdmenu
 			button.Click += delegate {
@@ -30,5 +33,4 @@ namespace roborally
 		}
 	}
 }
-
 
