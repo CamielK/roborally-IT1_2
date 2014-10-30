237395ce80d479c0c75e1f781cfcffcaf2ff476e
2 <2>
2014-10-30 20:41:35 +0100

    hoofdmenu afgemaakt

--- Resources/Resource.designer.cs
+++ Resources/Resource.designer.cs
@@ -67,16 +67,37 @@ namespace roborally
 		{
 			
 			// aapt resource value: 0x7f050003
-			public const int demoImageView = 2131034115;
-			
-			// aapt resource value: 0x7f050001
-			public const int editText1 = 2131034113;
+			public const int Feedback = 2131034115;
 			
 			// aapt resource value: 0x7f050000
-			public const int editText2 = 2131034112;
+			public const int Lobby_joinen = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int Lobby_maken = 2131034113;
 			
 			// aapt resource value: 0x7f050002
-			public const int myButton = 2131034114;
+			public const int Vriendenlijst = 2131034114;
+			
+			// aapt resource value: 0x7f05000a
+			public const int demoImageView = 2131034122;
+			
+			// aapt resource value: 0x7f050008
+			public const int editText1 = 2131034120;
+			
+			// aapt resource value: 0x7f050007
+			public const int editText2 = 2131034119;
+			
+			// aapt resource value: 0x7f050006
+			public const int instellingen = 2131034118;
+			
+			// aapt resource value: 0x7f050004
+			public const int linearLayout1 = 2131034116;
+			
+			// aapt resource value: 0x7f050009
+			public const int myButton = 2131034121;
+			
+			// aapt resource value: 0x7f050005
+			public const int uitloggen = 2131034117;
 			
 			static Id()
 			{
--- hoofdmenu.cs
+++ hoofdmenu.cs
@@ -13,6 +13,7 @@ using Android.Widget;
 
 namespace roborally
 {
+
 	[Activity (Label = "hoofdmenu")]			
 	public class hoofdmenu : Activity
 	{
@@ -22,10 +23,30 @@ namespace roborally
 
 			SetContentView (Resource.Layout.Hoofdmenu);
 
-			Button button = FindViewById<Button> (Resource.Id.uitloggen);
+			Button button_lobbyjoinen 	= FindViewById<Button> (Resource.Id.Lobby_joinen);
+			Button button_lobbymaken 	= FindViewById<Button> (Resource.Id.Lobby_maken);
+			Button button_vriendenlijst = FindViewById<Button> (Resource.Id.Vriendenlijst);
+			Button button_feedback 		= FindViewById<Button> (Resource.Id.Feedback);
+			Button button_uitloggen 	= FindViewById<Button> (Resource.Id.uitloggen);
+			Button button_instellingen 	= FindViewById<Button> (Resource.Id.instellingen);
 
-			button.Click += delegate {
+			button_lobbyjoinen.Click += delegate {
+				StartActivity(typeof(lobby_lijst));
+			};
+			button_lobbymaken.Click += delegate {
+				StartActivity(typeof(new_lobby));
+			};
+			button_vriendenlijst.Click += delegate {
+				StartActivity(typeof(vriendenlijst));
+			};
+			button_feedback.Click += delegate {
+				StartActivity(typeof(feedback));
+			};
+			button_uitloggen.Click += delegate {
 				StartActivity(typeof(MainActivity));
+			};
+			button_instellingen.Click += delegate {
+				StartActivity(typeof(instellingen));
 			};
 		}
 	}
