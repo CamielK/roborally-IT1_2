1d704cc1093ce577946c79b700dc9624cf966df4
2 <2>
2014-10-30 20:59:19 +0100

    feedback .cs en .axml aangemaakt en afgemaakt

--- Resources/Resource.designer.cs
+++ Resources/Resource.designer.cs
@@ -66,38 +66,44 @@ namespace roborally
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050003
-			public const int Feedback = 2131034115;
-			
-			// aapt resource value: 0x7f050000
-			public const int Lobby_joinen = 2131034112;
-			
-			// aapt resource value: 0x7f050001
-			public const int Lobby_maken = 2131034113;
-			
-			// aapt resource value: 0x7f050002
-			public const int Vriendenlijst = 2131034114;
-			
-			// aapt resource value: 0x7f05000a
-			public const int demoImageView = 2131034122;
-			
-			// aapt resource value: 0x7f050008
-			public const int editText1 = 2131034120;
-			
-			// aapt resource value: 0x7f050007
-			public const int editText2 = 2131034119;
-			
 			// aapt resource value: 0x7f050006
-			public const int instellingen = 2131034118;
+			public const int Feedback = 2131034118;
+			
+			// aapt resource value: 0x7f050003
+			public const int Lobby_joinen = 2131034115;
 			
 			// aapt resource value: 0x7f050004
-			public const int linearLayout1 = 2131034116;
-			
-			// aapt resource value: 0x7f050009
-			public const int myButton = 2131034121;
+			public const int Lobby_maken = 2131034116;
 			
 			// aapt resource value: 0x7f050005
-			public const int uitloggen = 2131034117;
+			public const int Vriendenlijst = 2131034117;
+			
+			// aapt resource value: 0x7f05000c
+			public const int demoImageView = 2131034124;
+			
+			// aapt resource value: 0x7f050001
+			public const int editText1 = 2131034113;
+			
+			// aapt resource value: 0x7f05000a
+			public const int editText2 = 2131034122;
+			
+			// aapt resource value: 0x7f050009
+			public const int instellingen = 2131034121;
+			
+			// aapt resource value: 0x7f050007
+			public const int linearLayout1 = 2131034119;
+			
+			// aapt resource value: 0x7f05000b
+			public const int myButton = 2131034123;
+			
+			// aapt resource value: 0x7f050000
+			public const int textView1 = 2131034112;
+			
+			// aapt resource value: 0x7f050008
+			public const int uitloggen = 2131034120;
+			
+			// aapt resource value: 0x7f050002
+			public const int verzenden = 2131034114;
 			
 			static Id()
 			{
@@ -113,10 +119,13 @@ namespace roborally
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Hoofdmenu = 2130903040;
+			public const int feedback = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Main = 2130903041;
+			public const int Hoofdmenu = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int Main = 2130903042;
 			
 			static Layout()
 			{
---
+++ feedback.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace roborally
+{
+	[Activity (Label = "Feedback geven")]			
+	public class feedback : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.feedback);
+
+			Button button_verzenden	= FindViewById<Button> (Resource.Id.verzenden);
+
+			button_verzenden.Click += delegate {
+				StartActivity(typeof(hoofdmenu));
+			};
+		}
+	}
+}
+
